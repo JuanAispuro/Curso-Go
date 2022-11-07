5de79a3817c582d74a165eb3cde6b58c38b7f516
JuanAispuro <Juan.aispuro193@tectijuana.edu.mx>
2022-11-07 12:10:37 -0800

    Arreglo

---
+++ paquetes y modulos/go.mod
@@ -0,0 +1,3 @@
+module github.com/JuanAispuro/Curso-Go
+
+go 1.19
--- paquetes y modulos/modulo.go
+++ paquetes y modulos/modulo.go
@@ -1,1 +1,10 @@
 package main 
+
+import (
+	"fmt"
+	"github.com/JuanAispuro/Curso-go/paquetes-y-modulos/greet"
+)
+func main (){
+	fmt.Println("Hello")
+	fmt.Println(greet.Italian())
+}
